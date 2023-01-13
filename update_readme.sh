c31ec8011d9f2ea22ddc71a075643e1ef3bb511d
James Dixon <notjamesdixon@gmail.com>
2023-01-13 17:53:01 -0500

    update shell script

--- update_readme.sh
+++ update_readme.sh
@@ -1,4 +1,15 @@
+#!/usr/bin/env bash
+
+# This is a script that injects a cowsay after README_template.md file
+# Run this from a cron job to update daily
+
+# move into repo directory
+cd "$HOME/Code/lemonase"
+
+# append fortune | cowsay command to README_template.md and out the actual README.md file
 TMPL="duck"
 cat README_template.md <(echo '```txt') <(echo "Daily ${TMPL} say for $(date +'%D')") <(fortune | cowsay -f $TMPL) <(echo '```') > README.md
+
+# add README.md, commit with message and push to GitHub
 git add README.md && git commit -m "Daily cowsay for $(date +"%Y-%m-%d")" && git push
 
