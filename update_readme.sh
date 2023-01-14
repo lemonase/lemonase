6655352aa5c24c36def67a52db03cbd06e9206e2
James Dixon <notjamesdixon@gmail.com>
2023-01-14 06:54:19 +0000

    update shell script with arguments and path detection

--- update_readme.sh
+++ update_readme.sh
@@ -6,8 +6,22 @@
 # Ex: run `crontab -e` and add the path to this script to run everyday at 5 min past midnight
 # 5 12 * * * '/path/to/repo/update_readme.sh'
 
-REPO_DIR="$HOME/Code/lemonase"
-COWSAY_TMPL="duck"
+if [ -z "$1" ]; then
+  if [ -d "$(dirname -- "$0")" ]; then
+    REPO_DIR="$(dirname -- "$(readlink -f  -- "$0";)";)"
+  else
+    echo "Please supply arg to specify path to repo with GitHub README"
+    exit
+  fi
+else
+  REPO_DIR="$1"
+fi
+
+if [ -z "$2" ]; then
+  COWSAY_TMPL="duck"
+else
+  COWSAY_TMPL="$2"
+fi
 
 cd "$REPO_DIR" || exit
 cat README_template.md <(echo '```txt') <(echo "Daily ${COWSAY_TMPL} say for $(date +'%D')") <(fortune | cowsay -f ${COWSAY_TMPL}) <(echo '```') > README.md
